311d3753df7b80b30701a318f7b268d912442442
OwaisAhmed18 <owaisahmed8736@gmail.com>
2024-08-26 23:20:00 +0530

    Navbar mode chnaged,Categories added to the navbar and  Customized buttons

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,12 @@ export class NewsItem extends Component {
     let {title, imgUrl, description, url} = this.props ;
     return (
       <> 
-        <div className="card" style={{width:'18rem'}}>
+        <div className="card">
           <img src={imgUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Read more</a>
+            <a href={url} target="_blank" rel="noreferrer" className="btn btn-dark">Read more</a>
           </div>
         </div>
       </>
