954b6554a71cebf35fa61ae09a57b7782df5d014
OwaisAhmed18 <owaisahmed8736@gmail.com>
2024-09-18 15:07:09 +0530

    Refactor News component to use default parameters instead of defaultProps. Reason: Addressed React warning about support for defaultProps being removed from function components in future major releases. Using default parameters instead.

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
+const News = ({ country = "in",
+  pageSize = 6,
+  category = "general",
+  apiKey,
+  setProgress,}) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [loadingMore, setLoadingMore] = useState(false);
@@ -14,30 +17,30 @@ const News = (props) => {
   useEffect(() => {
     document.title =
       "NewsWave - " +
-      props.category.charAt(0).toUpperCase() +
-      props.category.slice(1);
+      category.charAt(0).toUpperCase() +
+      category.slice(1);
     fetchArticles();
     // eslint-disable-next-line
   }, []);
 
   const fetchArticles = async () => {
-    props.setProgress(10);
+    setProgress(10);
     setLoading(true);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     const data = await fetch(url);
-    props.setProgress(30);
+    setProgress(30);
     const parsedData = await data.json();
-    props.setProgress(70);
+    setProgress(70);
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
-    props.setProgress(100);
+    setProgress(100);
   };
 
   const fetchMoreData = async () => {
     setLoadingMore(true);
     const nextPage = page + 1;
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${nextPage}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${nextPage}&pageSize=${pageSize}`;
     const data = await fetch(url);
     const parsedData = await data.json();
     setArticles(articles.concat(parsedData.articles));
@@ -50,7 +53,7 @@ const News = (props) => {
     <>
         <h2 className="container my-4">
           NewsWave | Top Headlines -{" "}
-          {props.category.charAt(0).toUpperCase() + props.category.slice(1)}
+          {category.charAt(0).toUpperCase() + category.slice(1)}
         </h2>
         {loading && <Spinner />}
 
@@ -89,20 +92,6 @@ const News = (props) => {
         </InfiniteScroll>
     </>
   );
-};
-
-News.defaultProps = {
-  country: "in",
-  pageSize: 6,
-  category: "general",
-};
-
-News.propTypes = {
-  country: PropTypes.string,
-  category: PropTypes.string,
-  pageSize: PropTypes.number,
-  apiKey: PropTypes.string.isRequired,
-  setProgress: PropTypes.func.isRequired,
 };
 
 export default News;
